03911356dff50c6c3a85ce955f02a3d15b1ed918
FinOCE <me@itsf.in>
2024-01-12 23:49:26 +1000

    Create WIP remaining CRUD triggers

---
+++ Blaze/Triggers/GetAllLinksTrigger.fs
@@ -0,0 +1,15 @@
+﻿namespace Blaze.Triggers
+
+open Microsoft.Azure.WebJobs
+open Blaze.Types
+open Microsoft.AspNetCore.Mvc
+open Microsoft.AspNetCore.Http
+open Microsoft.Azure.WebJobs.Extensions.Http
+
+module GetAllLinksTrigger =
+    [<FunctionName("GetAllLinksTrigger")>]
+    let Run
+        ([<HttpTrigger(AuthorizationLevel.Function, "get", Route = null)>] req: HttpRequest)
+        ([<CosmosDB("%CosmosConnectionString%", "links", DatabaseName = "links-db", Id = "{shortUrl}")>] link: Link) =
+            // TODO: Change above to create client or read all links
+            NotFoundResult() :> IActionResult
---
+++ Blaze/Triggers/GetLinkTrigger.fs
@@ -0,0 +1,14 @@
+﻿namespace Blaze.Triggers
+
+open Microsoft.Azure.WebJobs
+open Blaze.Types
+open Microsoft.AspNetCore.Mvc
+open Microsoft.AspNetCore.Http
+open Microsoft.Azure.WebJobs.Extensions.Http
+
+module GetLinkTrigger =
+    [<FunctionName("GetLinkTrigger")>]
+    let Run
+        ([<HttpTrigger(AuthorizationLevel.Function, "get", Route = "{shortUrl}")>] req: HttpRequest)
+        ([<CosmosDB("%CosmosConnectionString%", "links", DatabaseName = "links-db", Id = "{shortUrl}")>] link: Link) =
+            RedirectResult(link.DestinationUrl) :> IActionResult
