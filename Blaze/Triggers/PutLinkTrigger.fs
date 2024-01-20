d513153bb2c6c6b073a2c2334cff2db2f5ea2a28
Finley Sherwood <me@itsf.in>
2024-01-21 00:37:36 +1000

    Return 201 or 200 depending on if link already existed

--- Blaze/Triggers/PutLinkTrigger.fs
+++ Blaze/Triggers/PutLinkTrigger.fs
@@ -6,19 +6,31 @@ open Blaze.Types
 open Microsoft.AspNetCore.Mvc
 open Microsoft.Azure.WebJobs
 open Microsoft.Azure.WebJobs.Extensions.Http
+open System.Collections.Generic
 
 module PostLinkTrigger =
     [<FunctionName("PostLinkTrigger")>]
     let Run
         ([<HttpTrigger(AuthorizationLevel.Function, "put", Route = "{shortUrl}")>] destinationUrl: string)
+        ([<CosmosDB(containerName = "links", databaseName = "links-db", Connection = "CosmosConnectionString", Id = "{shortUrl}", PartitionKey = "{shortUrl}")>] existing: Link)
         ([<CosmosDB(containerName = "links", databaseName = "links-db", Connection = "CosmosConnectionString")>] db: Link outref)
         (shortUrl: string) =
-            let dto = new LinkDto(shortUrl, destinationUrl)
+            try
+                // Attempt to shortcircuit if the link already exists
+                if existing.DestinationUrl = destinationUrl then
+                    OkObjectResult(LinkMapper.FromDomain(existing)) :> IActionResult
+                else
+                    raise (KeyNotFoundException())
+            with | _ ->
+                // Create and validate dto
+                let dto = new LinkDto(shortUrl, destinationUrl)
             
-            match LinkMapper.ToDomain(dto) with
-            | Ok link ->
-                db <- link
-                OkObjectResult(LinkMapper.FromDomain(link)) :> IActionResult
-            | Error errors ->
-                BadRequestObjectResult(errors) :> IActionResult
+                match LinkMapper.ToDomain(dto) with
+                | Ok link ->
+                    // Add to database
+                    db <- link
+                    CreatedResult($"/{link.ShortUrl}", LinkMapper.FromDomain(link)) :> IActionResult
+                | Error errors ->
+                    // Return errors
+                    BadRequestObjectResult(errors) :> IActionResult
                 
