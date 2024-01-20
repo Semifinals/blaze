fd47224cd1839842dcb2f6ede69ee78e41aa841f
Finley Sherwood <me@itsf.in>
2024-01-21 01:43:32 +1000

    Create robots.txt to disallow crawling

---
+++ Blaze/Triggers/SeoTrigger.fs
@@ -0,0 +1,12 @@
+﻿namespace Blaze.Triggers
+
+open Microsoft.AspNetCore.Http
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Azure.WebJobs
+open Microsoft.Azure.WebJobs.Extensions.Http
+
+module SeoTrigger =
+    [<FunctionName("SeoTrigger")>]
+    let Run ([<HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "robots.txt")>] req: HttpRequest) =
+        OkObjectResult("User-agent: *\nDisallow: /")
+        
