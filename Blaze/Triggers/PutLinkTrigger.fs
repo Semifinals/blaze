779b6bbaa9c6e67d4dffe22636ba9efa4dc1bf54
Fin <me@itsf.in>
2024-02-07 15:50:29 +1000

    Fix missing response body documentation

--- Blaze/Triggers/PutLinkTrigger.fs
+++ Blaze/Triggers/PutLinkTrigger.fs
@@ -16,11 +16,11 @@ module PostLinkTrigger =
     [<Operation(Summary = "Create a link", Description = "Creates a new short link, or replaces an existing one")>]
     [<Parameter("shortUrl", In = ParameterLocation.Path, Required = true, Type = typeof<string>, Summary = "The short URL", Description = "Defines the short URL for the short link to create")>]
     [<RequestBody("text/plain", typeof<string>, Required = true, Description = "The destination URL the short URL should redirect to")>]
+    [<ResponseBody(HttpStatusCode.OK, "application/json", typeof<LinkDto>, Summary = "Successful request, already exists", Description = "Indicates the request was successful but that the existing value is identical")>]
+    [<ResponseBody(HttpStatusCode.Created, "application/json", typeof<LinkDto>, Summary = "Successful request, created or updated", Description = "Indicates the short URL was successfully created or updated")>]
     [<ResponseBody(HttpStatusCode.BadRequest, "application/json", typeof<string list>, Summary = "Bad request", Description = "Occurs when either the short URL or destination URL are not valid")>]
     [<Response(HttpStatusCode.Unauthorized, Summary = "Unauthorized request", Description = "Occurs when the authentication header is missing or invalid")>]
     [<Response(HttpStatusCode.Forbidden, Summary = "Forbidden request", Description = "Occurs when authorization failed due to missing permissions")>]
-    [<Response(HttpStatusCode.OK, Summary = "Successful request, already exists", Description = "Indicates the request was successful but that the existing value is identical")>]
-    [<Response(HttpStatusCode.Created, Summary = "Successful request, created or updated", Description = "Indicates the short URL was successfully created or updated")>]
     let Run
         ([<HttpTrigger(AuthorizationLevel.Function, "put", Route = "{shortUrl}")>] destinationUrl: string)
         ([<CosmosDB(containerName = "links", databaseName = "links-db", Connection = "CosmosConnectionString", Id = "{shortUrl}", PartitionKey = "{shortUrl}")>] existing: Link)
